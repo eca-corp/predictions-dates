dd69ecdce42d400be63a932d6c9144ab66034be4
ecadav <ecacorp13@gmail.com>
2022-10-05 00:08:03 -0500

    first intercation

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3 --t
+# -*- encoding: utf-8 -*-
+__author__ = "Emanuel Cortes Antonio"
+__copyrigth__ = "copyright 2022, SFBB Predictions with ML"
+__credits__ = "ecadav"
+__licence__ = "GPL"
+__Version__ = "1.1.2"
+__maintainer__ = "Community"
+__email__ = "ecadav@gmail.com"
+__status__ == "Jr Python"
+
+
